f629528ffb419f181681b9b56616ec49c20bfdfc
Esdras Bezerra da Silva <esdras.silva@cps.sp.gov.br>
2026-09-11 21:08:56 -0300

    Quote Initial

--- lib/src/pages/quote_page.dart
+++ lib/src/pages/quote_page.dart
@@ -13,9 +13,23 @@ class _QuotePageState extends State<QuotePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('DummyJson Quote')),
-        //TODO: Utilizar o FutureBuilder para transacionar a requisição REST e controlar o estado da UI
-        body: null
+      appBar: AppBar(title: Text('DummyJson Quote')),
+      //TODO: Utilizar o FutureBuilder para transacionar a requisição REST e controlar o estado da UI
+      body: Center(
+        child: Card(
+          margin: .all(16),
+          child: Padding(
+            padding: .all(16.0),
+            child: Text(
+              'Mussum ipsum cacildis vidis litrus abertis',
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.large(
+        onPressed: () {},
+        child: Icon(Icons.format_quote_outlined),
+      ),
     );
   }
 }
